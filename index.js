1ae0f5413673193a8e1097b25a772c1787bffd63
Prince Parimal <145918970+SanuParimal@users.noreply.github.com>
2023-09-24 21:37:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+let birthdayWish = [
+
+    "You are like a diamond, stunning and rare, and I'm so lucky to have you in my life.",
+    "Your smile is like sunshine on a cloudy day, it brightens up everything around you.",
+    "You are the definition of elegance and grace, a true masterpiece.",
+    "In a world full of trends, you remain real, timeless, and classic.",
+    "Your kindness and compassion make you even more beautiful on the inside than you are on the outside.",
+    "You are not just an artist; you are a work of art, a living masterpiece.",
+    "Your laughter is the most beautiful sound in the world.",
+    "The way you carry yourself with confidence and poise is truly inspiring.",
+    "You have a heart that shines brighter than any star in the sky.",
+    "Your presence alone is a gift to everyone around you.",
+    "The world is a more beautiful place with you in it.",
+    "Your strength and resilience make you even more captivating.",
+    "Your inner beauty radiates and enhances your outer beauty."
+
+]
+let c = Math.floor(Math.random() * 13)
+document.getElementById("birthdayWish").innerHTML = birthdayWish[c]
+
+let birthdayLine = [
+    "Sisters are like flowers in the garden of life, each one unique and beautiful in her own way.",
+    "A sister is both your mirror – and your opposite.",
+    "Having a sister is like having a best friend you can't get rid of. You know whatever you do, they'll still be there.",
+    "Sisters may drive you crazy, get into your stuff, and irritate you. However, if anyone else dares say so, a sister will defend you to the death.",
+    "Sisters share the scent and smells – the feel of a common childhood.",
+    "Sisterhood is a special kind of friendship that never fades, no matter the distance or time apart.",
+    "A sister is both your confidante and your conspirator against all odds.",
+    "Sisters are the sunshine that lights up your days, even in the darkest of times.",
+    "A sister is a gift to the heart, a friend to the spirit, and a golden thread to the meaning of life.",
+    "In the cookies of life, sisters are the chocolate chips.",
+    "Sisters make the bad times good and the good times unforgettable.",
+    "Sisters are like stars. You may not always see them, but you know they're always there.",
+    "No matter where life takes us, the bonds of sisterhood will never break."
+
+]
+
+let d = Math.floor(Math.random() * 13)
+document.getElementById("birthdayLine").innerHTML = birthdayLine[d]
+
